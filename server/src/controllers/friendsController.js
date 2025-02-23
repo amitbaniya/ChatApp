59b6fb85f025f16435108a54445ce5ca615c7d60
Amit Baniya <122513988+amitbaniya@users.noreply.github.com>
2025-02-23 22:25:21 +0545

    Searching fix
    Searching with first name, username, lastname
    Fixed default image path

--- server/src/controllers/friendsController.js
+++ server/src/controllers/friendsController.js
@@ -3,32 +3,30 @@ import User from "../models/User.js";
 export const findFriends = async (req, res) => {
   try {
     const { searchTerm } = req.query;
-    console.log(req.query);
+    console.log("Search term:", searchTerm);
     if (!searchTerm) {
       return res.status(400).json({ message: "No search Term" });
     }
 
-    const textUsers = await User.find({
-      $text: { $search: searchTerm },
-    }).sort({ score: { $meta: "textScore" } });
-
-    const regexUsers = await User.find({
-      username: { $regex: searchTerm, $options: "i" },
+    const users = await User.find({
+      $or: [
+        { username: { $regex: searchTerm, $options: "i" } },
+        { firstname: { $regex: searchTerm, $options: "i" } },
+        { lastname: { $regex: searchTerm, $options: "i" } },
+        {
+          $expr: {
+            $regexMatch: {
+              input: { $concat: ["$firstname", " ", "$lastname"] },
+              regex: searchTerm,
+              options: "i",
+            },
+          },
+        },
+      ],
     });
-
-    const users = [...textUsers, ...regexUsers].filter(
-      (user, index, self) =>
-        index ===
-        self.findIndex((u) => u._id.toString() === user._id.toString())
-    );
-
-    if (!users) {
-      return res.status(400).json({ message: "No users found" });
-    }
 
     res.status(200).json({ users });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
