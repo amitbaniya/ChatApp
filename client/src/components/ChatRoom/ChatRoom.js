bde85c3b23abf4bc3a7d7c83f1e292621a51103c
Amit Baniya <122513988+amitbaniya@users.noreply.github.com>
2025-02-26 13:14:39 +0545

    Unauthorized loadup redirection to home page
    Messages are not shown if the user is not part of chat room

--- client/src/components/ChatRoom/ChatRoom.js
+++ client/src/components/ChatRoom/ChatRoom.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./ChatRoom.css";
 import { PROFILE_URL } from "../../services/Constants";
 import { Avatar, Input } from "antd";
@@ -13,14 +13,18 @@ import {
 } from "../../services/ChatServices";
 import { useAuth } from "../../context/AuthContext";
 import Messages from "./components/Messages";
+import { useNavigate } from "react-router-dom";
 
 function ChatRoom() {
   const { chatRoomId } = useParams();
   const { currentChat, setCurrentChat, messages, setMessages, addMessage } =
     useChat();
   const { user } = useAuth();
-
+  const navigate = useNavigate();
   const [messageInput, setMessageInput] = useState("");
+  const [isMember, setIsMember] = useState(false);
+  const chatContainerRef = useRef(null);
+
   useEffect(() => {
     const fetchChatRoomData = async () => {
       const friendId = await handleChatRoomData(chatRoomId);
@@ -30,12 +34,23 @@ function ChatRoom() {
     if (!currentChat || messages.length === 0) {
       fetchChatRoomData();
     }
-  }, [chatRoomId]);
+  }, [chatRoomId]); // eslint-disable-line react-hooks/exhaustive-deps
 
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
   const handleChatRoomData = async (chatRoomId) => {
     try {
       const chatRoomData = await getChatRoomData(chatRoomId);
-
+      if (chatRoomData.members.includes(user.id)) {
+        setIsMember(true);
+      } else {
+        setIsMember(false);
+        navigate("/");
+      }
       if (user.id === chatRoomData.members[0]) {
         return chatRoomData.members[1];
       } else {
@@ -81,37 +96,41 @@ function ChatRoom() {
         "Loading"
       ) : (
         <>
-          <div className="chatHeader">
-            <Avatar
-              className="chatProfile"
-              style={{
-                "--profile-bg": `url(${PROFILE_URL}/default.png)`,
-              }}
-            ></Avatar>
+          {isMember && (
+            <>
+              <div className="chatHeader">
+                <Avatar
+                  className="chatProfile"
+                  style={{
+                    "--profile-bg": `url(${PROFILE_URL}/default.png)`,
+                  }}
+                ></Avatar>
 
-            <h1 className="chatName">
-              {" "}
-              {`${currentChat.firstname} ${currentChat.lastname}`}
-            </h1>
-          </div>
-          <div className="chatContainer">
-            {messages.length !== 0 && <Messages messages={messages} />}
-          </div>
-          <div className="messageInputContainer">
-            <Input
-              placeholder="Send message"
-              className="messageInput"
-              value={messageInput}
-              onChange={(e) => setMessageInput(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  e.preventDefault();
-                  handleSend();
-                }
-              }}
-            ></Input>
-            <SendOutlined className="sendButton" onClick={handleSend} />
-          </div>
+                <h1 className="chatName">
+                  {" "}
+                  {`${currentChat.firstname} ${currentChat.lastname}`}
+                </h1>
+              </div>
+              <div className="chatContainer" ref={chatContainerRef}>
+                {messages.length !== 0 && <Messages messages={messages} />}
+              </div>
+              <div className="messageInputContainer">
+                <Input
+                  placeholder="Send message"
+                  className="messageInput"
+                  value={messageInput}
+                  onChange={(e) => setMessageInput(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      e.preventDefault();
+                      handleSend();
+                    }
+                  }}
+                ></Input>
+                <SendOutlined className="sendButton" onClick={handleSend} />
+              </div>
+            </>
+          )}
         </>
       )}
     </>
