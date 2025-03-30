7ebeb65e7e167b1cbcc56a606dda245a27c5c0a8
Amit Baniya <122513988+amitbaniya@users.noreply.github.com>
2025-03-30 11:25:27 +0545

    Deployment fix 2

--- client/src/services/Constants.js
+++ client/src/services/Constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://chat-app-eight-tan.vercel.app/";
+export const API_URL = "https://chatapp-ll9o.onrender.com/";
 export const PROFILE_URL =
   "https://chat-app-eight-tan.vercel.app/uploads/profiles";
