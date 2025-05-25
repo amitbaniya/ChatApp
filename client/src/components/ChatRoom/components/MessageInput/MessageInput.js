bdbf0b1c09612ea6a0c07b7b954a20029fc983b8
Amit Baniya <122513988+amitbaniya@users.noreply.github.com>
2025-05-25 11:44:38 +0545

    Image Uploader
    Image uploaded setup for cloudinary

--- client/src/components/ChatRoom/components/MessageInput/MessageInput.js
+++ client/src/components/ChatRoom/components/MessageInput/MessageInput.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./MessageInput.css";
 import { Input } from "antd";
-import { SendOutlined } from "@ant-design/icons";
+import { PictureOutlined, SendOutlined } from "@ant-design/icons";
+import ImageUploader from "../../../ImageUploader/ImageUploader";
+import { useState } from "react";
 
 function MessageInput({ handleSend, messageInput, setMessageInput }) {
+  const [imageUrl, setImageUrl] = useState(null);
+  const onImageUpload = (url) => {
+    // Send publicId to backend
+    setImageUrl(url); 
+    console.log(url)
+  };
   return (
     <div className="messageInputContainer">
       <Input
@@ -18,6 +26,7 @@ function MessageInput({ handleSend, messageInput, setMessageInput }) {
           }
         }}
       ></Input>
+      <ImageUploader onUpload={onImageUpload} icon={<PictureOutlined className="uploadButton" />} />
       <SendOutlined className="sendButton" onClick={handleSend} />
     </div>
   );
