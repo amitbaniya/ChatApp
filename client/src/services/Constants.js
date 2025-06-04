dccda0ef372ab05fffca8b1ff7cf050d6c506e70
Amit Baniya <122513988+amitbaniya@users.noreply.github.com>
2025-06-04 17:16:00 +0545

    Deployment fix
    Url can be changed from env

--- client/src/services/Constants.js
+++ client/src/services/Constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://chatapp-ll9o.onrender.com/";
+export const API_URL = process.env.REACT_APP_API_URL;
 export const PROFILE_URL =
   "https://chat-app-eight-tan.vercel.app/uploads/profiles";
